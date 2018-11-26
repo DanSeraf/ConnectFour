937c2fa4a8f186cfcd914e048338656c7daf91a0
DanSeraf <seraf.daniele@gmail.com>
2018-11-26 17:14:58 +0100

    inputstream in costructor

--- src/it/unicam/cs/pa/player/HumanPlayer.java
+++ src/it/unicam/cs/pa/player/HumanPlayer.java
@@ -5,6 +5,7 @@ import it.unicam.cs.pa.core.DiscColors;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class HumanPlayer extends AbstractPlayer implements Player {
@@ -12,10 +13,16 @@ public class HumanPlayer extends AbstractPlayer implements Player {
     private static final long serialVersionUID = 6446382602942139248L;
     private String username;
     private Disc disc;
+    private BufferedReader reader;
 
-    public HumanPlayer(char symbol, String username, DiscColors color) {
+    public HumanPlayer(char symbol, String username, DiscColors color, InputStream in) {
         this.username = username;
         this.disc = new Disc(symbol, color);
+        this.reader = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public HumanPlayer(char symbol, String username, DiscColors color) {
+        this(symbol, username, color, System.in);
     }
 
     @Override
