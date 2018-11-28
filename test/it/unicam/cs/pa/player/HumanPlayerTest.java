70879e29f78d5d3050a7e99a227eb942704a2522
DanSeraf <seraf.daniele@gmail.com>
2018-11-28 10:14:45 +0100

    Human player test

--- test/it/unicam/cs/pa/player/HumanPlayerTest.java
+++ test/it/unicam/cs/pa/player/HumanPlayerTest.java
@@ -4,20 +4,29 @@ import it.unicam.cs.pa.core.DiscColors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HumanPlayerTest {
 
     @Test
-    @DisplayName("Human Player creation")
-    void createPlayer() {
-        char symbol = '#';
-        DiscColors disc_color = DiscColors.BLUE;
-        String playername = "Pluto";
-        HumanPlayer p1 = new HumanPlayer(symbol, playername, disc_color);
+    @DisplayName("Human player creation")
+    void createHumanPlayer() {
+        HumanPlayer p1 = new HumanPlayer('#', "Stallino", DiscColors.BLUE, System.in);
+        assertEquals("Stallino", p1.getUser());
+        assertEquals('#', p1.getDisc().getSymbol());
+        assertEquals(DiscColors.BLUE.toString(), p1.getDisc().getColor());
         assertNotNull(p1);
-        assertEquals(playername, p1.getUser());
-        assertEquals(symbol, p1.getDisc().getSymbol());
-        assertEquals(disc_color.toString(), p1.getDisc().getColor());
+    }
+
+    @Test
+    @DisplayName("Get move")
+    void getMove() throws IOException {
+        String data = "3";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        HumanPlayer testplayer = new HumanPlayer('#', "Pluto", DiscColors.GREEN, System.in);
+        assertEquals(3, testplayer.getMove());
     }
 }
