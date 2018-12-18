0c9219a04b3c1b54e86731991b500ad4e397d76e
DanSeraf <seraf.daniele@gmail.com>
2018-12-18 12:34:54 +0100

    removed abstract disc

--- src/it/unicam/cs/pa/core/AbstractDisc.java
+++
@@ -1,7 +0,0 @@
-package it.unicam.cs.pa.core;
-
-import java.io.Serializable;
-
-abstract class AbstractDisc implements Serializable {
-    private static final long serialVersionUID = 5180726400657075678L;
-}
