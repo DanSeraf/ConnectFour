f179bfb8936c5587de0c67fec14c861467c31b73
DanSeraf <seraf.daniele@gmail.com>
2019-01-10 16:29:12 +0100

    removed test

--- test/it/unicam/cs/pa/core/SettingsTest.java
+++ test/it/unicam/cs/pa/core/SettingsTest.java
@@ -37,18 +37,4 @@ class SettingsTest {
         Settings settings = new Settings(System.in, System.out);
         settings.addNewPlayer();
     }
-
-    @Test
-    @DisplayName("NumberFormatException correctly thrown when generated a new player with incorrect input")
-    void NfeOnAddNewPlayer() {
-        // Check number format exception on disc color selection
-        String inputdata = "Pluto\ntt\n@\n2";
-        System.setIn(new ByteArrayInputStream(inputdata.getBytes()));
-        Settings settings1 = new Settings(System.in, System.out);
-        assertThrows(NumberFormatException.class, () -> settings1.addNewPlayer());
-        // Check number format exception on select type of player
-        String inputdata1 = "Pluto\n2\n#\nbb";
-        System.setIn(new ByteArrayInputStream(inputdata1.getBytes()));
-        Settings settings2 = new Settings(System.in, System.out);
-    }
 }
