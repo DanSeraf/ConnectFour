666097737ce5f902dda99cb3ae25a05752f1d7ae
DanSeraf <seraf.daniele@gmail.com>
2019-09-03 21:58:59 +0200

    unicode box enum (used in Console)

---
+++ src/main/java/it/unicam/cs/pa/core/UnicodeBox.java
@@ -0,0 +1,35 @@
+package it.unicam.cs.pa.core;
+
+public enum UnicodeBox {
+    EMPTYCELL,
+    PIPE,
+    SIDE,
+    URCORNER,
+    ULCORNER,
+    DRCORNER,
+    DLCORNER,
+    MSEPARATOR,
+    LSEPARATOR,
+    RSEPARATOR,
+    USEPARATOR,
+    DSEPARATOR;
+
+    @Override
+    public String toString() {
+        switch ( this ) {
+            case EMPTYCELL: return "│   ";
+            case PIPE: return "│";
+            case SIDE: return "───";
+            case URCORNER: return "┐";
+            case ULCORNER: return "┌───";
+            case DRCORNER: return "┘";
+            case DLCORNER: return "└───";
+            case MSEPARATOR: return "┼───";
+            case LSEPARATOR: return "├───";
+            case RSEPARATOR: return "┤";
+            case USEPARATOR: return "┬───";
+            case DSEPARATOR: return "┴───";
+        }
+        return "";
+    }
+}
